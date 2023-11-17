93b80c3dbebd892eb939122d259bc552c370431d
damienbod <damien_bod@hotmail.com>
2023-11-17 19:44:02 +0100

    fix signout

--- ui/Pages/Logout.cshtml.cs
+++ ui/Pages/Logout.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,10 +10,11 @@ namespace WebCodeFlowPkceClient.Pages;
 [Authorize]
 public class LogoutModel : PageModel
 {
-    public async Task<IActionResult> OnGetAsync()
+    public IActionResult OnGet()
     {
-        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
-        return Redirect("/SignedOut");
+        return SignOut(
+            new AuthenticationProperties { RedirectUri = "/SignedOut" },
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            OpenIdConnectDefaults.AuthenticationScheme);
     }
 }
