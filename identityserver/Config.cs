a9de0b2da28c2aa8022ce000ecdc388e7c72ae3f
damienbod <damien_bod@hotmail.com>
2025-05-09 08:59:18 +0200

    Update break changes

--- identityserver/Config.cs
+++ identityserver/Config.cs
@@ -5,21 +5,16 @@ namespace IdentityServer;
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
+        [
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-        };
+        ];
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-        {
-            //new ApiScope("scope-new")
-        };
+        [];
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
-        {
+        [
             new Client
             {
                 ClientId = "web-par",
@@ -36,5 +31,5 @@ public static class Config
                 AllowOfflineAccess = true,
                 AllowedScopes = { "openid", "profile" }
             }
-        };
+        ];
 }
